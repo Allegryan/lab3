3cafa0c09cd8eefbc2d85250181da337d05a0800
Allegryan <ryanb101803@gmail.com>
2023-04-19 18:27:30 -0700

    fixes

---
+++ ListTests.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListTests{
+
+    @Test
+    public void testFilter(){
+        List<String> list = new ArrayList<String>();
+        list.add("test");
+        list.add("tes");
+        list.add("te");
+        list.add("te1");
+        ListExamples sc = new ListExamples();
+
+        assertArrayEquals(new String[]{"tes", "te1"}, ListExamples.filter(list, sc).toArray());
+
+    }
+}
